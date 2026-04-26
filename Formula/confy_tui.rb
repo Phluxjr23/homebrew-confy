3805fac33470ce2178e089337af91e8833956caa
phluxjr <noreply@phluxjr.net>
2026-04-25 21:29:47 -0500

    do smt idk anymore man

--- Formula/confy_tui.rb
+++ Formula/confy_tui.rb
@@ -1,5 +1,7 @@
 class ConfyTui < Formula
-  desc "a config manager for linux, unix, bsd, and macos systems"
+  include Language::Python::Virtualenv
+
+  desc "tui config manager for linux, unix, bsd, and macos systems"
   homepage "https://phluxjr.net/confy"
   url "https://files.pythonhosted.org/packages/source/c/confy-tui/confy_tui-2.1.2.tar.gz"
   sha256 "a5659ff035994175c9333c9740f08c398f3eb2a2fb832c28869d2e1134f22a9e"
@@ -8,7 +10,11 @@ class ConfyTui < Formula
   depends_on "python@3.11"
 
   def install
-    bin.install "confy_tui/confy_tui.py" => "confy"
+    virtualenv_install_with_resources
+  end
+
+  def test
+    # basic sanity check: CLI exists and runs
+    assert_match "confy", shell_output("#{bin}/confy --help")
   end
 end
-
